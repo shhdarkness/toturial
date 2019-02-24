1032e21c7c27f427795cee39fe90a660c6fa96ba
shhdarkness2 <47919667+shhdarkness2@users.noreply.github.com>
2019-02-24 11:03:01 +0330

    Update MainActivity.java

--- main/java/com/example/toturial/MainActivity.java
+++ main/java/com/example/toturial/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+public void test (){
+    
+}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
